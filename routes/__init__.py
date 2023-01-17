1bb9c80173d3af09b67369ef387fd12ee014ad9b
Astral <jenson27@Hotmail.com>
2023-01-18 00:37:46 +0800

    Inventory Converted to use SQL Alchemy

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,8 +3,7 @@
 # from routes.{python-file} import {variable}
 # Import routes here
 from routes.inventory import inventory
-
-
+from .database import db
 from flask_uploads import configure_uploads, IMAGES, UploadSet
 from uuid import UUID
 import time
@@ -26,6 +25,11 @@ app.config["SECRET_KEY"] = "mysuperdupersecrethash365tothepowerof369andadditiono
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOADED_SHOP_DEST'] = os.path.join(localpath, 'static/shop')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(localpath, "data.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 shopphotos = UploadSet("shop", IMAGES)
 configure_uploads(app, shopphotos)
