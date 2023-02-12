be1b6326a2d5b7554c4822cdd3014a1e37100c1a
Astral <jenson27@Hotmail.com>
2023-02-12 20:19:59 +0800

    Implemented shop page

---
+++ routes/shop.py
@@ -0,0 +1,30 @@
+import uuid
+from flask import Blueprint, render_template, request, redirect, url_for, abort, flash, send_from_directory, escape
+from data.database import Database
+from data.table import Table
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, validators, FileField, IntegerField, SelectField, SubmitField
+from wtforms.validators import InputRequired
+from werkzeug.utils import secure_filename
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+import os
+import re
+
+shop = Blueprint("shop", __name__, static_folder=os.path.join(
+    os.getcwd(), "static"), template_folder=os.path.join(os.getcwd(), "templates"))
+
+
+@shop.route('/', methods=["GET"])
+def get_shop():
+    """
+    Shows full shop and inventory
+    """
+    db = Database()
+    try:
+        table = db.tables["inventory"]
+    except KeyError:
+        table = Table('imagename', 'name', 'category',
+                      'description', 'quantity', 'price', 'productID')
+    products = table.rows
+    db.close()
+    return render_template('shop.html', products=products)
